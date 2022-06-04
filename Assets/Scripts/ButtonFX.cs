ea58bf910735596a88098247dbb3e1515eb38587
Ebooooo <maticover2@gmail.com>
2022-06-04 19:26:37 +0200

    Whole_game

---
+++ Assets/Scripts/ButtonFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFX);
+    }
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFX);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+       public void LoadNextScene()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
